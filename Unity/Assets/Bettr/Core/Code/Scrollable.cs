d86dd8e8448b4010bd01eed345a3f4d18de48727
Ron Vergis <ron.vergis@gmail.com>
2024-02-24 14:05:48 -0600

    updates to horizontal and vertical scrolling

--- Unity/Assets/Bettr/Core/Code/Scrollable.cs
+++ Unity/Assets/Bettr/Core/Code/Scrollable.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 // ReSharper disable once CheckNamespace
 namespace Bettr.Core
@@ -11,29 +13,74 @@ namespace Bettr.Core
         public float scrollSpeed = 0.5f;
         public float minY = 0f;
         public float maxY = 0f;
+        public float minX = 0f;
+        public float maxX = 0f;
         public bool vertical = true;
+        public UnityEvent onScrollBegin;
+        public UnityEvent onScrollEnd;
         [NonSerialized] private Vector3 _previousMousePosition;
+        [NonSerialized] private bool _isScrolling = false;
+        [NonSerialized] private bool _isPointerOverScrollable = false;
 
         void Update()
         {
             if (Input.GetMouseButtonDown(0))
             {
-                // Record the position of the mouse when the button is first pressed
-                _previousMousePosition = Input.mousePosition;
+                if (IsPointerOverGameObject())
+                {
+                    _isPointerOverScrollable = true;
+                    // Record the position of the mouse when the button is first pressed
+                    _previousMousePosition = Input.mousePosition;
+                    // Invoke the onScrollBegin event
+                    if (!_isScrolling)
+                    {
+                        onScrollBegin.Invoke();
+                        _isScrolling = true;
+                    }
+                }
             }
             else if (Input.GetMouseButton(0))
             {
-                // Calculate the distance the mouse has moved since the last frame
-                Vector3 delta = Input.mousePosition - _previousMousePosition;
-                _previousMousePosition = Input.mousePosition;
+                if (_isPointerOverScrollable)
+                {
+                    // Calculate the distance the mouse has moved since the last frame
+                    Vector3 delta = Input.mousePosition - _previousMousePosition;
+                    _previousMousePosition = Input.mousePosition;
 
-                // Translate the Background plane along its local Y axis
-                var localPosition = scrollable.transform.localPosition;
-                localPosition = new Vector3(localPosition.x, Mathf.Clamp(localPosition.y + delta.y * scrollSpeed, minY, maxY), localPosition.z);
-                scrollable.transform.localPosition = localPosition;
-
-                Debug.Log($"scrollable.transform.localPosition={localPosition}");
+                    // Translate the scrollable GameObject
+                    var localPosition = scrollable.transform.localPosition;
+                    if (vertical)
+                    {
+                        localPosition.y = Mathf.Clamp(localPosition.y + delta.y * scrollSpeed, minY, maxY);
+                    }
+                    else // If not vertical, assume horizontal scrolling.
+                    {
+                        localPosition.x = Mathf.Clamp(localPosition.x + delta.x * scrollSpeed, minX, maxX);
+                    }
+                    scrollable.transform.localPosition = localPosition;
+                }
             }
+            else if (_isScrolling)
+            {
+                // Mouse button is not down, if we were scrolling, invoke the onScrollEnd event
+                onScrollEnd.Invoke();
+                _isScrolling = false;
+                _isPointerOverScrollable = false;
+            }
+        }
+
+        private bool IsPointerOverGameObject()
+        {
+            PointerEventData eventData = new PointerEventData(EventSystem.current);
+            eventData.position = Input.mousePosition;
+            LayerMask layerMask = LayerMask.GetMask(LayerMask.LayerToName(gameObject.layer));
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                return hit.collider.gameObject == scrollable || hit.collider.transform.IsChildOf(scrollable.transform);
+            }
+            return false;
         }
     }
 }
