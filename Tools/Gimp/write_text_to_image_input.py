b43810a8e8647881c6781b74245f071605384275
Ron Vergis <ron.vergis@gmail.com>
2024-06-15 10:59:58 -0500

    update assets

--- Tools/Gimp/write_text_to_image_input.py
+++ Tools/Gimp/write_text_to_image_input.py
@@ -61,6 +61,42 @@ inputs = [
         ]
     },
     {
+        "input_filename": 'DefaultLobbyCard.jpg',
+        "output_filename": 'Game001__MysticalLegends__DefaultLobbyCard.jpg',
+        "font_name": 'Verdana',
+        "font_size": 200,
+        "text_color": (255, 255, 255),
+        "background_color": "#F08080",
+        "lines": [
+            ("MYSTICAL", 'Center', 'Center', 0, -200),
+            ("LEGENDS", 'Center', 'Center', 0, 200),
+        ]
+    },
+    {
+        "input_filename": 'DefaultLobbyCard.jpg',
+        "output_filename": 'Game001__AncientAdventures__DefaultLobbyCard.jpg',
+        "font_name": 'Verdana',
+        "font_size": 200,
+        "text_color": (255, 255, 255),
+        "background_color": "#F08080",
+        "lines": [
+            ("ANCIENT", 'Center', 'Center', 0, -200),
+            ("ADVENTURES", 'Center', 'Center', 0, 200),
+        ]
+    },
+    {
+        "input_filename": 'DefaultLobbyCard.jpg',
+        "output_filename": 'Game001__CosmicVoyage__DefaultLobbyCard.jpg',
+        "font_name": 'Verdana',
+        "font_size": 200,
+        "text_color": (255, 255, 255),
+        "background_color": "#F08080",
+        "lines": [
+            ("COSMIC", 'Center', 'Center', 0, -200),
+            ("VOYAGE", 'Center', 'Center', 0, 200),
+        ]
+    },    
+    {
         "input_filename": 'LobbyCard.jpg',
         "output_filename": 'Game001__GuardiansOfOlympus__LobbyCard.jpg',
         "font_name": 'Verdana',
@@ -121,126 +157,40 @@ inputs = [
             ("FORTUNE", 'Center', 'Center', 0, 20),
         ]
     },
-
-    {
-        "input_filename": 'DefaultLobbyCard.jpg',
-        "output_filename": 'Game001__GuardiansOfOlympus__DefaultLobbyCard.jpg',
-        "font_name": 'Verdana',
-        "font_size": 200,
-        "text_color": (255, 255, 255),
-        "background_color": "#98FB98",
-        "lines": [
-            ("GUARDIANS OF", 'Center', 'Center', 0, -200),
-            ("OLYMPUS", 'Center', 'Center', 0, 200),
-        ]
-    },
-    {
-        "input_filename": 'DefaultLobbyCard.jpg',
-        "output_filename": 'Game001__AtlantisTreasures__DefaultLobbyCard.jpg',
-        "font_name": 'Verdana',
-        "font_size": 200,
-        "text_color": (255, 255, 255),
-        "background_color": "#F08080",
-        "lines": [
-            ("ATLANTIS", 'Center', 'Center', 0, -200),
-            ("TREASURES", 'Center', 'Center', 0, 200),
-        ]
-    },
-    {
-        "input_filename": 'DefaultLobbyCard.jpg',
-        "output_filename": 'Game001__ClockworkChronicles__DefaultLobbyCard.jpg',
-        "font_name": 'Verdana',
-        "font_size": 200,
-        "text_color": (255, 255, 255),
-        "background_color": "#AAF0D1",
-        "lines": [
-            ("CLOCKWORK", 'Center', 'Center', 0, -200),
-            ("CHRONICLES", 'Center', 'Center', 0, 200),
-        ]
-    },
-    {
-        "input_filename": 'DefaultLobbyCard.jpg',
-        "output_filename": 'Game001__GalacticQuest__DefaultLobbyCard.jpg',
-        "font_name": 'Verdana',
-        "font_size": 200,
-        "text_color": (255, 255, 255),
-        "background_color": "#9AC4FF",
-        "lines": [
-            ("GALACTIC", 'Center', 'Center', 0, -200),
-            ("QUEST", 'Center', 'Center', 0, 200),
-        ]
-    },
-    {
-        "input_filename": 'DefaultLobbyCard.jpg',
-        "output_filename": 'Game001__PharosFortune__DefaultLobbyCard.jpg',
-        "font_name": 'Verdana',
-        "font_size": 200,
-        "text_color": (255, 255, 255),
-        "background_color": "#F08080",
-        "lines": [
-            ("PHAROS", 'Center', 'Center', 0, -200),
-            ("FORTUNE", 'Center', 'Center', 0, 200),
-        ]
-    },
     {
         "input_filename": 'LobbyCard.jpg',
-        "output_filename": 'Game001__GuardiansOfOlympus__LobbyCard.jpg',
-        "font_name": 'Verdana',
-        "font_size": 20,
-        "text_color": (255, 255, 255),
-        "background_color": "#98FB98",
-        "lines": [
-            ("GUARDIANS", 'Center', 'Center', 0, -28),
-            ("OF", 'Center', 'Center', 0, 0),
-            ("OLYMPUS", 'Center', 'Center', 0, 24),
-        ]
-    },
-    {
-        "input_filename": 'LobbyCard.jpg',
-        "output_filename": 'Game001__AtlantisTreasures__LobbyCard.jpg',
+        "output_filename": 'Game001__MysticalLegends__LobbyCard.jpg',
         "font_name": 'Verdana',
         "font_size": 20,
         "text_color": (255, 255, 255),
         "background_color": "#F08080",
         "lines": [
-            ("ATLANTIS", 'Center', 'Center', 0, -20),
-            ("TREASURES", 'Center', 'Center', 0, 20),
+            ("MYSTICAL", 'Center', 'Center', 0, -20),
+            ("LEGENDS", 'Center', 'Center', 0, 20),
         ]
     },
     {
         "input_filename": 'LobbyCard.jpg',
-        "output_filename": 'Game001__ClockworkChronicles__LobbyCard.jpg',
-        "font_name": 'Verdana',
-        "font_size": 18,
-        "text_color": (255, 255, 255),
-        "background_color": "#AAF0D1",
-        "lines": [
-            ("CLOCKWORK", 'Center', 'Center', 0, -20),
-            ("CHRONICLES", 'Center', 'Center', 0, 20),
-        ]
-    },
-    {
-        "input_filename": 'LobbyCard.jpg',
-        "output_filename": 'Game001__GalacticQuest__LobbyCard.jpg',
-        "font_name": 'Verdana',
-        "font_size": 20,
-        "text_color": (255, 255, 255),
-        "background_color": "#9AC4FF",
-        "lines": [
-            ("GALACTIC", 'Center', 'Center', 0, -20),
-            ("QUEST", 'Center', 'Center', 0, 20),
-        ]
-    },
-    {
-        "input_filename": 'LobbyCard.jpg',
-        "output_filename": 'Game001__PharosFortune__LobbyCard.jpg',
+        "output_filename": 'Game001__AncientAdventures__LobbyCard.jpg',
         "font_name": 'Verdana',
         "font_size": 20,
         "text_color": (255, 255, 255),
         "background_color": "#F08080",
         "lines": [
-            ("PHAROS", 'Center', 'Center', 0, -20),
-            ("FORTUNE", 'Center', 'Center', 0, 20),
+            ("ANCIENT", 'Center', 'Center', 0, -20),
+            ("ADVENTURES", 'Center', 'Center', 0, 20),
+        ]
+    },
+    {
+        "input_filename": 'LobbyCard.jpg',
+        "output_filename": 'Game001__CosmicVoyage__LobbyCard.jpg',
+        "font_name": 'Verdana',
+        "font_size": 20,
+        "text_color": (255, 255, 255),
+        "background_color": "#F08080",
+        "lines": [
+            ("COSMIC", 'Center', 'Center', 0, -20),
+            ("VOYAGE", 'Center', 'Center', 0, 20),
         ]
     },
     {
