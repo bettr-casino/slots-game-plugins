e245f2abab50b58f794500701e63c1c5e2046af6
Ron Vergis <ron.vergis@gmail.com>
2024-09-22 11:03:05 -0500

    update audio calls

--- Unity/Assets/Bettr/Core/Code/BettrAudioController.cs
+++ Unity/Assets/Bettr/Core/Code/BettrAudioController.cs
@@ -1,5 +1,6 @@
 using System;
 using CrayonScript.Code;
+using UnityEngine;
 
 // ReSharper disable once CheckNamespace
 namespace Bettr.Core
@@ -13,6 +14,55 @@ namespace Bettr.Core
         {
             TileController.RegisterType<BettrAudioController>("BettrAudioController");
             TileController.AddToGlobals("BettrAudioController", this);
+        }
+
+        public void PlayAudioOnce(GameObject gameObjectWithAudioSource, string audioClipName)
+        {
+            // get the audio source components
+            var audioSources = gameObjectWithAudioSource.GetComponents<AudioSource>();
+            // find the audio source with the audio clip "spinbutton"
+            foreach (var audioSource in audioSources)
+            {
+                if (audioSource.clip.name == audioClipName)
+                {
+                    audioSource.loop = false;
+                    // play the audio clip
+                    audioSource.Play();
+                    break;
+                }
+            }
+        }
+        
+        public void PlayAudioLoop(GameObject gameObjectWithAudioSource, string audioClipName)
+        {
+            // get the audio source components
+            var audioSources = gameObjectWithAudioSource.GetComponents<AudioSource>();
+            // find the audio source with the audio clip "spinbutton"
+            foreach (var audioSource in audioSources)
+            {
+                if (audioSource.clip.name == audioClipName)
+                {
+                    audioSource.loop = true;
+                    // play the audio clip
+                    audioSource.Play();
+                    break;
+                }
+            }
+        }
+        
+        public void StopAudio(GameObject gameObjectWithAudioSource, string audioClipName)
+        {
+            // get the audio source components
+            var audioSources = gameObjectWithAudioSource.GetComponents<AudioSource>();
+            // find the audio source with the audio clip "spinbutton"
+            foreach (var audioSource in audioSources)
+            {
+                if (audioSource.clip.name == audioClipName)
+                {
+                    audioSource.Stop();
+                    break;
+                }
+            }
         }
     }
 }
