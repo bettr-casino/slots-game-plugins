a03d85960060891ca4fbc587a82448b1ff54e05b
Ron Vergis <ron.vergis@gmail.com>
2024-10-14 17:33:50 -0500

    PlayBackgroundVideo

--- Unity/Assets/Bettr/Core/Code/BettrVideoController.cs
+++ Unity/Assets/Bettr/Core/Code/BettrVideoController.cs
@@ -19,6 +19,11 @@ namespace Bettr.Core
         
         public static BettrVideoController Instance { get; private set; }
         
+        public bool VideoPreparationComplete { get; private set; }
+        public bool VideoPreparationError { get; private set; }
+        
+        public bool VideoLoopPointReached { get; private set; }
+        
         public void Awake()
         {
             Instance = this;
@@ -44,6 +49,72 @@ namespace Bettr.Core
                 yield break;
             }
             
+            // Create the URL for the background video
+            var backgroundVideoName = $"{machineName}{machineVariant}BackgroundVideo";
+            var assetUrl = $"{VideoServerBaseURL}/video/latest/{backgroundVideoName}.mp4";
+            
+            // set the url of the video player to the assetUrl
+            videoPlayer.url = assetUrl;
+            
+            VideoPreparationComplete = false;
+            VideoPreparationError = false;
+            
+            // Prepare the video player
+            videoPlayer.Prepare();
+            // Add a listener for when the video is prepared
+            videoPlayer.prepareCompleted += source =>
+            {
+                Debug.Log($"VideoPlayer loaded successfully machineName={machineName}, machineVariant={machineVariant}, experimentVariant={experimentVariant}");
+                VideoPreparationComplete = true;
+                VideoPreparationError = false;
+            };
+            
+            // Add a listener for when there is an error
+            videoPlayer.errorReceived += (VideoPlayer vp, string message) =>
+            {
+                Debug.Log($"VideoPlayer Error: {message} machineName={machineName}, machineVariant={machineVariant}, experimentVariant={experimentVariant}");
+                VideoPreparationComplete = true;
+                VideoPreparationError = true;
+            };
+            
+            while (!VideoPreparationComplete)
+            {
+                yield return null;
+            }
+        }
+
+        public IEnumerator PlayBackgroundVideo(Table backgroundTable, string machineName, string machineVariant, string experimentVariant)
+        {
+            if (VideoPreparationError)
+            {
+                Debug.Log($"Failed to load background video machineName={machineName}, machineVariant={machineVariant}, experimentVariant={experimentVariant}");
+                yield break;
+            }
+            
+            if (!VideoPreparationComplete)
+            {
+                Debug.Log($"Background video not yet prepared machineName={machineName}, machineVariant={machineVariant}, experimentVariant={experimentVariant}");
+                yield break;
+            }
+            
+            // Get the BackgroundFBX property from the backgroundTable
+            var backgroundFBX = backgroundTable["BackgroundFBX"] as PropertyGameObject;
+            if (backgroundFBX == null)
+            {
+                // log info and quietly fail
+                Debug.Log($"Failed to load backgroundFBX from backgroundTable machineName={machineName}, machineVariant={machineVariant}, experimentVariant={experimentVariant}");
+                yield break;
+            }
+            
+            // get the VideoPlayer component from the backgroundFBX
+            var videoPlayer = backgroundFBX.gameObject.GetComponent<VideoPlayer>();
+            // if null, log and quietly fail
+            if (videoPlayer == null)
+            {
+                Debug.Log($"Failed to load VideoPlayer from backgroundFBX machineName={machineName}, machineVariant={machineVariant}, experimentVariant={experimentVariant}");
+                yield break;
+            }
+            
             // get the mesh renderer from the backgroundFBX
             var meshRenderer = backgroundFBX.gameObject.GetComponent<MeshRenderer>();
             // if null, log and quietly fail
@@ -51,11 +122,7 @@ namespace Bettr.Core
             {
                 Debug.Log($"Failed to load MeshRenderer from backgroundFBX machineName={machineName}, machineVariant={machineVariant}, experimentVariant={experimentVariant}");
                 yield break;
-            }
-            
-            // Create the URL for the background video
-            var backgroundVideoName = $"{machineName}{machineVariant}BackgroundVideo";
-            var assetUrl = $"{VideoServerBaseURL}/video/latest/{backgroundVideoName}.mp4";
+            }         
             
             // get the cached asset bundle
             // lowercase everything for safety
@@ -71,21 +138,25 @@ namespace Bettr.Core
             
             // get the material from the asset bundle. The material name will be called BackgroundVideoMaterial
             var material = cachedAssetBundle.LoadAsset<Material>("BackgroundVideoMaterial");
-
-            // set the url of the video player to the assetUrl
-            videoPlayer.url = assetUrl;
             
-            // Prepare the video player
-            videoPlayer.Prepare();
-            // Add a listener for when the video is prepared
-            videoPlayer.prepareCompleted += source =>
-            {
-                // switch the material to the render BackgroundVideoMaterial material
-                meshRenderer.material = material;
+            // switch the material to the render BackgroundVideoMaterial material
+            meshRenderer.material = material;
+            
+            VideoLoopPointReached = false;
                 
-                // Play the video
-                videoPlayer.Play();
+            // Play the video
+            videoPlayer.Play();
+            
+            // Add a listener for when the video finishes playing
+            videoPlayer.loopPointReached += source =>
+            {
+                VideoLoopPointReached = true;
             };
+            
+            while (!VideoLoopPointReached)
+            {
+                yield return null;
+            }
         }
     }
 }
