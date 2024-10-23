5601d94dbd4b770ed5149db9c23cfa4d3e54adbf
Ron Vergis <ron.vergis@gmail.com>
2024-10-23 10:18:59 -0500

    lobby cards - rearrange png

--- temp_scripts/convert-lobby-cards-to-jpg.sh
+++ temp_scripts/convert-lobby-cards-to-jpg.sh
@@ -7,6 +7,13 @@ target_dir="/Users/rvergis/Documents/External/Bettr/GitHub/bettr-casino/slots-ga
 
 for file in $(find $target_dir -name "Game*.png"); do
     echo "Converting $file to ${file%.*}.jpg"
-    # use magick to convert and save the file at the same relative path
-    magick "$file" -quality 85 "${file%.*}.jpg"
+    # find the parent directory of the file
+    parent_dir=$(dirname $file)
+    # its parent is Textures
+    textures_dir=$(dirname $parent_dir)
+    # mkdir the jpg directory under Textures
+    mkdir -p "$textures_dir/jpg"
+    # use magick to convert and save the file to the jpg directory    
+    target_file="$textures_dir/jpg/$(basename ${file%.*}.jpg)"
+    magick "$file" -quality 85 "$target_file"
 done
--- temp_scripts/rearrange-lobby-cards-textures.sh
+++ temp_scripts/rearrange-lobby-cards-textures.sh
@@ -5,29 +5,13 @@
 
 target_dir="/Users/rvergis/Documents/External/Bettr/GitHub/bettr-casino/slots-game-plugins/Unity/Assets/Bettr/Runtime/Plugin/LobbyCard"
 
-for file in $(find $target_dir -name "Game*.png.meta"); do
+for file in $(find $target_dir -name "Game*.png"); do
     # find the parent directory of the file
     parent_dir=$(dirname $file)
     # make a new directories  Textures/png at the same level as the parent directory
     mkdir -p "$parent_dir/../Textures/png"
     # move the file to the new directory
     mv "$file" "$parent_dir/../Textures/png"
-done
-
-for file in $(find $target_dir -name "Game*.webp.meta"); do
-    # find the parent directory of the file
-    parent_dir=$(dirname $file)
-    # make a new directories  Textures/webp at the same level as the parent directory
-    mkdir -p "$parent_dir/../Textures/webp"
-    # move the file to the new directory
-    mv "$file" "$parent_dir/../Textures/webp"
-done
-
-for file in $(find $target_dir -name "Game*.jpg.meta"); do
-    # find the parent directory of the file
-    parent_dir=$(dirname $file)
-    # make a new directories  Textures/webp at the same level as the parent directory
-    mkdir -p "$parent_dir/../Textures/jpg"
-    # move the file to the new directory
-    mv "$file" "$parent_dir/../Textures/jpg"
+    # move the meta file to the new directory
+    mv "${file}.meta" "$parent_dir/../Textures/png"
 done
